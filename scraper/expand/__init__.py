52b2d562208152c58e6cc139ea3f2fdedf2694d4
HobnobMancer <eemh1@st-andrews.ac.ul>
2021-05-31 16:37:18 +0100

    move duplicate functions to a single module

--- scraper/expand/__init__.py
+++ scraper/expand/__init__.py
@@ -37,3 +37,73 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 """A module for expanding the the local CAZY database beyond what is provided within CAZy."""
+
+
+import logging
+import json
+import sys
+
+from tqdm import tqdm
+
+
+def get_cazy_dict(args):
+    """Retrieve dict of CAZy family annotations of proteins."""
+    logger = logging.getLogger(__name__)
+
+    try:
+        with open(args.dict, "r") as fh:
+            cazy_dict = json.load(fh)
+
+    except FileNotFoundError:
+        logger.error(
+            "Did not find the local CAZy dict (JSON) file.\n"
+            "Check the path is correct.\n"
+            "Terminating programme"
+        )
+        sys.exit(1)
+
+    return cazy_dict
+
+
+def get_qualifying_proteins(cazy_dict, config_data):
+    """Identify proteins to retrieve sequences for, those that meet at least one config criteria.
+
+    :param cazy_dict: dict of proteins catalogued in CAZy
+        Keyed by protein GenBank accession, valued by list of CAZy family annotations
+    :param config_data: dict of two sets: CAZy classes and families to retrieve seqs for
+
+    Return list of proteins (1 protein = 1 GenBank acession) to retrieve seqs for.
+    """
+    proteins = set()  # proteins to retrieve sequences for, set prevents duplicates
+
+    if (len(list(config_data["classes"])) == 0) and (len(list(config_data["families"])) != 0):
+        # check only in family configuration data
+        for protein in tqdm(cazy_dict, desc="Identifying CAZymes matching config data"):
+            for fam in cazy_dict[protein]:
+                if fam in config_data["families"]:
+                    proteins.add(protein)
+                    continue
+    
+    elif (len(list(config_data["classes"])) != 0) and (len(list(config_data["families"])) == 0):
+        # check only class configuration data
+        for protein in tqdm(cazy_dict, desc="Identifying CAZymes matching config data"):
+            for cazy_class in config_data["classes"]:
+                for fam in cazy_dict[protein]:
+                    if fam.startswith(cazy_class):
+                        proteins.add(protein)
+                        continue
+    
+    else:
+        # check both configuration data
+        for protein in tqdm(cazy_dict, desc="Identifying CAZymes matching config data"):
+            for fam in cazy_dict[protein]:
+                if fam in config_data["families"]:
+                    proteins.add(protein)
+                    continue
+            for cazy_class in config_data["classes"]:
+                for fam in cazy_dict[protein]:
+                    if fam.startswith(cazy_class):
+                        proteins.add(protein)
+                        continue
+    
+    return list(proteins)
