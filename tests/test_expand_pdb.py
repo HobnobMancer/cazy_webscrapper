30653220fed4fb03646b0e9dfa95a9f01a435db7
HobnobMancer <eemh1@st-andrews.ac.uk>
2021-06-11 15:21:52 +0100

    unit test download

--- tests/test_expand_pdb.py
+++ tests/test_expand_pdb.py
@@ -49,6 +49,8 @@ import pytest
 
 from argparse import Namespace, ArgumentParser
 
+from Bio.PDB import PDBList
+
 from scraper import utilities
 from scraper.expand.get_pdb_structures import get_pdb_structures
 from scraper.sql import sql_orm
@@ -87,6 +89,34 @@ def args_no_db():
         database="Fake_path"
     )}
     return args
+
+
+@pytest.fixture
+def args_no_outdir():
+    args = {"args": Namespace(
+        batch_limit=4,
+        outdir=None,
+        overwrite=False,
+        pdb="pdb,xml",
+    )}
+    return args
+
+
+
+@pytest.fixture
+def args_outdir(test_dir):
+    path_ = test_dir / "test_outputs"
+    path_ = path_ / "test_outputs_expand"
+    args = {"args": Namespace(
+        batch_limit=4,
+        outdir=path_,
+        overwrite=False,
+        pdb="pdb,xml",
+    )}
+    return args
+
+
+# test main()
 
 
 def test_main_args(args_parser, monkeypatch):
@@ -444,3 +474,32 @@ def test_get_all_pdb_accessions(db_session):
     """Test get_all_pdb_accessions"""
 
     get_pdb_structures.get_all_pdb_accessions(db_session)
+
+
+# test download_pdb_structures()
+
+
+def test_download_none(args_no_outdir, monkeypatch):
+    """Test download_pdb_structures when args.outdir is None"""
+
+    def mock_PDBList(*args, **kwargs):
+        return
+    
+    monkeypatch.setattr(PDBList, "download_pdb_files", mock_PDBList)
+
+    accession_list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+
+    get_pdb_structures.download_pdb_structures(accession_list, args_no_outdir['args'])
+
+
+def test_download(args_outdir, monkeypatch):
+    """Test download_pdb_structures when args.outdir is not None"""
+
+    def mock_PDBList(*args, **kwargs):
+        return
+    
+    monkeypatch.setattr(PDBList, "download_pdb_files", mock_PDBList)
+
+    accession_list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+
+    get_pdb_structures.download_pdb_structures(accession_list, args_outdir['args'])
